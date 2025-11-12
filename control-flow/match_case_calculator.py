4cfbae1975ddf3a1655a559f754509dd6c48822c
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-11-13 00:42:50 +0200

    final enhance

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,38 +1,20 @@
-# simple cal
-f_num = float(input("Enter the first number:"))
-s_num =float(input("Enter the second number:"))
-operator = input("Choose the operation (+, -, *, /):")
-# # operation
-
-#
-# if operator== "+":
-#     print(f"The result is {f_num + s_num}")
-# elif operator == "-":
-#     print(f"The result is {f_num - s_num}")
-# elif operator == "*":
-#     print(f"The result is {f_num * s_num}")
-# elif operator == "/":
-#     print(f"The result is {f_num / s_num}")
-# elif s_num ==0 and operator == "/":
-#     print("Cannot divide by zero.")
-
-"""
-will never be reached, because the earlier elif operator == "/" runs first — and Python will already try to divide by zero,
-causing a crash (ZeroDivisionError) before that line executes.
-"""
 # match_case_calculator.py
 
-match operator:
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
     case "+":
-        print(f"The result is {f_num + s_num}")
+        print(f"The result is {num1 + num2}")
     case "-":
-        print(f"The result is {f_num - s_num}")
+        print(f"The result is {num1 - num2}")
     case "*":
-        print(f"The result is {f_num * s_num}")
+        print(f"The result is {num1 * num2}")
     case "/":
-        if s_num == 0:
+        if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            print(f"The result is {f_num / s_num}")
+            print(f"The result is {num1 / num2}")
     case _:
         print("Invalid operation selected.")
