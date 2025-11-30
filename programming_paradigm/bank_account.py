2d717ef72732b97409695b8d112965e554d1a9c1
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-11-30 02:41:34 +0200

    small fix to the out put from int to float

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -27,4 +27,4 @@ class BankAccount:
 
     def display_balance(self):
         """Print the current balance in the required format."""
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
