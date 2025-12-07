5507c755917a8d097f53439e33fd46a2aadf8db7
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-12-07 19:05:38 +0200

    refinement  to the related issue

--- oop/main.py
+++ oop/main.py
@@ -55,17 +55,3 @@
 #
 # if __name__ == "__main__":
 #     main()
-""" this code is related to the task before that """
-from class_static_methods_demo import Calculator
-
-def main():
-    # Using the static method
-    sum_result = Calculator.add(10, 5)
-    print(f"The sum is: {sum_result}")
-
-    # Using the class method
-    product_result = Calculator.multiply(10, 5)
-    print(f"The product is: {product_result}")
-
-if __name__ == "__main__":
-    main()
